4384521f724b5f59fd28a55ef37e7c9b90312cc3
pawelkom88e <pawel.komorkiewicz@yahoo.co.uk>
2023-02-25 11:40:14 +0000

    chore: Change component name

--- public/icons/ShoppingBagIcon.tsx
+++ public/icons/ShoppingBagIcon.tsx
@@ -1,4 +1,4 @@
-export default function ShoppingBag({size = 25}) {
+export default function ShoppingBagIcon({ size = 25 }) {
   return (
     <svg width={size} viewBox="0 0 16 16" aria-hidden="true" focusable="false" role="presentation">
       <path d="M15.07 5.023h-2.452V4.54a4.617 4.617 0 0 0-9.233 0v.482H.93L-.072 16h16.144zM4.883 4.54a3.118 3.118 0 0 1 6.233 0v.482H4.883zM1.572 14.5l.73-7.977h11.4l.73 7.977z"></path>
