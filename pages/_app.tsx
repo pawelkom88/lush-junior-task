e06e1e8384396ed1e067989d25fd7c37580b6c11
pawelkom88e <pawel.komorkiewicz@yahoo.co.uk>
2023-02-25 11:45:34 +0000

    refactor: Move state up

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,18 +1,23 @@
+import { useState } from "react";
 import type { AppProps } from "next/app";
 import { ApolloProvider } from "@apollo/client";
 import { client } from "./client/client";
-import "@fontsource/figtree";
-import "@styles/globals.scss";
 import Navigation from "@components/navigation/Navigation";
 import Footer from "@components/footer/Footer";
+import FooterMobile from "@components/footer/FooterMobile";
+import "@fontsource/figtree";
+import "@styles/globals.scss";
 
 export default function App({ Component, pageProps }: AppProps) {
+  const [keyword, setKeyword] = useState<string>("");
+
   return (
     // Implement a provider on a highest lvl in app, so other components(children) have access to it.
     <ApolloProvider client={client}>
-      <Navigation />
-      <Component {...pageProps} />
+      <Navigation onSetKeyword={setKeyword} />
+      <Component {...pageProps} keyword={keyword} />
       <Footer />
+      <FooterMobile />
     </ApolloProvider>
   );
 }
