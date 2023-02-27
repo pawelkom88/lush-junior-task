f32193016e0e36ae5e4703175ebf6be50ed7bcc0
pawelkom88e <pawel.komorkiewicz@yahoo.co.uk>
2023-02-27 09:42:01 +0000

    chore: Refactor types and utils files

--- types/types.ts
+++ types/types.ts
@@ -1,3 +1,27 @@
-export type ChildrenType = {
-    children: React.ReactNode;
+export type Props = {
+  children: React.ReactNode;
+};
+
+export type SearchBarProps = {
+  onSetKeyword: (val: string) => void;
+  onShowSearchField: () => void;
+  isDisplayed: boolean;
+};
+
+export interface ProductDescription {
+  id: string;
+  data: {
+    text: string;
+    type: string;
   };
+}
+
+export type ThumbnailProps = {
+  media: { url: string }[] | null | undefined;
+  alt: string;
+  onSelectedImage: (url: string) => void;
+};
+
+export interface SrButtonProps extends Props {
+  text: string;
+}
