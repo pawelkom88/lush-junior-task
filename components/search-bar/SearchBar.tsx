02d333c07b1e417c4ff60820c6c38603760a0237
pawelkom88e <pawel.komorkiewicz@yahoo.co.uk>
2023-02-27 09:44:56 +0000

    feat: Add resetQuery method / update styles

--- components/search-bar/SearchBar.tsx
+++ components/search-bar/SearchBar.tsx
@@ -1,27 +1,33 @@
 import { useState } from "react";
-// import { useState, Dispatch, SetStateAction } from "react";
+import { useRouter } from "next/router";
 import classes from "./SearchBar.module.scss";
 import ClearInputFieldIcon from "@root/public/icons/ClearInputFieldIcon";
+import { SearchBarProps } from "types/types";
 
-type SearchBarProps = {
-  //   setKeyword: Dispatch<SetStateAction<string>>;
-  onSetKeyword: (val: string) => void;
-  onClick: () => void;
-  isDisplayed: boolean;
-};
-
-export default function SearchBar({ isDisplayed, onSetKeyword, onClick }: SearchBarProps) {
+export default function SearchBar({
+  isDisplayed,
+  onSetKeyword,
+  onShowSearchField,
+}: SearchBarProps) {
+  const router = useRouter();
   const [value, setValue] = useState<string>("");
 
   function onSubmit(e: React.FormEvent<EventTarget>) {
     e.preventDefault();
+    router.push("/#products");
     onSetKeyword(value);
+  }
+
+  function resetQuery(): void {
+    onShowSearchField();
+    onSetKeyword("");
+    setValue("");
   }
 
   return (
     <form
       role="search"
-      className={`${classes.form} ${isDisplayed ? classes.mobile : classes.desktop}`}
+      className={`${classes.form} ${isDisplayed ? classes.onMobile : classes.onDesktop}`}
       onSubmit={onSubmit}>
       <label className="sr-only" htmlFor="search">
         Search <input type="text" />
@@ -35,10 +41,12 @@ export default function SearchBar({ isDisplayed, onSetKeyword, onClick }: Search
         className={`${isDisplayed ? classes.fullwidth : ""}`}
         onChange={e => setValue(e.target.value)}
       />
-      <input type="submit" style={{ display: "none" }} />
-      <button type="button" title="close" className={classes["clear-input"]} onClick={onClick}>
-        <ClearInputFieldIcon />
-      </button>
+      <input className={classes["hidden-input"]} type="submit" />
+      {value && (
+        <button type="button" title="close" className={classes["clear-input"]} onClick={resetQuery}>
+          <ClearInputFieldIcon />
+        </button>
+      )}
     </form>
   );
 }
