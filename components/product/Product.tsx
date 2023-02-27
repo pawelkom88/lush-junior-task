a619526642352050fa0669e5201835e509284de7
pawelkom88e <pawel.komorkiewicz@yahoo.co.uk>
2023-02-27 09:46:49 +0000

    refactor: Refactor Product components

--- components/product/Product.tsx
+++ components/product/Product.tsx
@@ -1,46 +1,63 @@
-import Image from "next/image";
+import { useState } from "react";
+import Image, { StaticImageData } from "next/image";
 import Link from "next/link";
-import classes from "./Product.module.scss";
+import Rating from "@components/product/rating/Rating";
+import Thumbnail from "@components/product/thumbnail/Thumbnail";
+import CartButton from "./cart-button/CartButton";
+import { productUnavailable, notAvailable } from "constants/constants";
+import { createSanitizedMarkup } from "utils/utils";
+import { ProductBySlugQuery } from "@generated/api";
+import { ProductDescription } from "types/types";
+import classes from "./product.module.scss";
 
-type ProductProps = {
-  product: {
-    name: string;
-    thumbnail: {
-      url: string;
-    };
-    slug: string;
-  };
-};
+type ProductPrice = { __typename?: "Money"; amount: number; currency: string } | undefined;
 
-export default function Product({ product }: ProductProps) {
-  const { name, thumbnail, category, slug, media, pricing } = product;
+export default function Product({ product }: ProductBySlugQuery) {
+  const { name, media, description, rating, pricing, weight } = product || {};
 
-  const amount = pricing?.priceRange.start.gross;
-  const price = amount.amount;
-  const deduction = pricing?.discount === null ? 0 : pricing?.discount.gross.amount;
+  const [selectedImage, setSelectedImage] = useState<string | StaticImageData>(
+    media?.[0]?.url || productUnavailable
+  );
 
-  const PriceAfterDiscount = price - deduction;
-  const PriceBeforeDiscount = price + deduction;
-  const currency = amount.currency;
-  const discountedPrice =
-    PriceBeforeDiscount === PriceAfterDiscount ? "" : `${PriceAfterDiscount} ${currency}`;
+  const productDescription: ProductDescription[] = JSON.parse(description).blocks;
 
-  const imageSrc = thumbnail ? thumbnail?.url : "/public/images/PRODUCT-UNAVAILABLE.png";
+  const productWeightWithUnit: string = weight ? `${weight.value} ${weight.unit}` : notAvailable;
+
+  const productPrice: ProductPrice = pricing?.priceRange?.start?.gross;
+
+  const productAmountWithCurrency: string = productPrice
+    ? `${productPrice.amount} ${productPrice.currency}`
+    : notAvailable;
 
   return (
-    <li className={classes.product}>
-      <div className={classes["product-image"]}>
-        <Image width={200} height={200} src={imageSrc} alt={name as string} />
-      </div>
-      <div className={classes["product-description"]}>
-        <p className={classes["product-name"]}>{name.split("-")}</p>
-        <p className={classes["product-description"]}>{category?.name}</p>
-        <s>{discountedPrice}</s>
-        <p>{`${PriceAfterDiscount} ${currency}`}</p>
-      </div>
-      <Link className={classes["product-details"]} href={`/product/${slug}`}>
-        details
+    <section>
+      <Link className={classes["homepage-link"]} href="/">
+        Back to products
       </Link>
-    </li>
+      <article className={classes.product}>
+        <div className={classes["product-wrapper"]}>
+          <div className={classes["product-image"]}>
+            <Image width={600} height={200} src={selectedImage} alt={name || ""} />
+          </div>
+          <Thumbnail alt={name || ""} media={media} onSelectedImage={setSelectedImage} />
+        </div>
+        <div className={classes["product-details"]}>
+          <h1 className={classes["product-details__name"]}>{name}</h1>
+          <Rating productRating={rating} />
+          <div className={classes["product-details__info"]}>
+            <strong>{productAmountWithCurrency}</strong>
+            <strong>
+              Weight: <span>{productWeightWithUnit}</span>
+            </strong>
+          </div>
+          <>
+            {productDescription?.map(({ id, data }: any) => {
+              return <p key={id} dangerouslySetInnerHTML={createSanitizedMarkup(data.text)} />;
+            })}
+          </>
+          {productPrice?.amount ? <CartButton /> : ""}
+        </div>
+      </article>
+    </section>
   );
 }
