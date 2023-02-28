5a4f09326fc814b43b8feb3e4f0b2a88508ff21d
pawelkom88e <pawel.komorkiewicz@yahoo.co.uk>
2023-02-28 09:51:49 +0000

    chore: Remove unused component

--- components/select/Select.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import classes from "./Select.module.scss";
-import { Props } from "types/types";
-
-export default function Select({children}:Props) {
-  return <div className={classes["select-wrapper"]}>{children}</div>;
-}
