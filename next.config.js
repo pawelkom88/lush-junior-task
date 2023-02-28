bd3549d67afa468441bb5ef2238eeaf3827e0f5c
pawelkom88e <pawel.komorkiewicz@yahoo.co.uk>
2023-02-28 12:13:34 +0000

    fix: Fix load more functionality

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: [process.env.DOMAIN],
+    domains: ["twstg2.eu.saleor.cloud"],
   },
 };
 
--- pages/client/client.tsx
+++ pages/client/client.tsx
@@ -1,6 +1,15 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
+import { relayStylePagination } from "@apollo/client/utilities";
 
 export const client = new ApolloClient({
   uri: process.env.NEXT_PUBLIC_APOLLO_CLIENT_URI,
-  cache: new InMemoryCache(),
+  cache: new InMemoryCache({
+    typePolicies: {
+      Query: {
+        fields: {
+          products: relayStylePagination([]),
+        },
+      },
+    },
+  }),
 });
