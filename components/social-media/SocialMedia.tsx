e8d40a9a229e0c10a83d8ee408eadf5b8f798c85
pawelkom88e <pawel.komorkiewicz@yahoo.co.uk>
2023-03-01 10:36:38 +0000

    Refactor: Remove unnecessary optional chaining

--- components/social-media/SocialMedia.tsx
+++ components/social-media/SocialMedia.tsx
@@ -5,7 +5,7 @@ import classes from "./SocialMedia.module.scss";
 export default function SocialMedia() {
   return (
     <div className={classes["footer-media"]}>
-      {socialMediaConfig?.map(({ id, src, alt }) => {
+      {socialMediaConfig.map(({ id, src, alt }) => {
         return <Image key={id} width={25} height={25} src={src} alt={alt} />;
       })}
     </div>
