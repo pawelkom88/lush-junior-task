4adecdd91660489d935d27dc9b4a97aaf86a15e9
pawelkom88e <pawel.komorkiewicz@yahoo.co.uk>
2023-03-05 22:45:12 +0000

    feat: Add cart functionality

--- components/product/Product.tsx
+++ components/product/Product.tsx
@@ -1,18 +1,31 @@
+import router from "next/router";
 import { useState } from "react";
 import Image, { StaticImageData } from "next/image";
 import Link from "next/link";
 import Rating from "@components/product/rating/Rating";
 import Thumbnail from "@components/product/thumbnail/Thumbnail";
-import CartButton from "./cart-button/CartButton";
+import Button from "../button/Button";
 import { productUnavailable, notAvailable } from "constants/constants";
 import { createSanitizedMarkup } from "utils/utils";
-import { ProductBySlugQuery } from "@generated/api";
+import { ProductBySlugQuery, useProductAddVariantToCartMutation } from "@generated/api";
 import { ProductDescription } from "types/types";
+import { useLocalStorage } from "react-use";
 import classes from "./product.module.scss";
 
-type ProductPrice = { __typename?: "Money"; amount: number; currency: string } | undefined;
+type ProductPrice = { amount: number; currency: string } | undefined;
 
 export default function Product({ product }: ProductBySlugQuery) {
+  const [token] = useLocalStorage("token");
+  const [addProductToCart] = useProductAddVariantToCartMutation();
+
+  const selectedVariantID: string = product?.variants?.[0]?.id ?? "";
+
+  async function onAddToCart() {
+    await addProductToCart({
+      variables: { checkoutToken: token, variantId: selectedVariantID as string },
+    });
+  }
+
   const { name = "", media, description, rating, pricing, weight, isAvailable } = product || {};
 
   const [selectedImage, setSelectedImage] = useState<string | StaticImageData>(
@@ -43,7 +56,7 @@ export default function Product({ product }: ProductBySlugQuery) {
         </div>
         <div className={classes["product-details"]}>
           <h1 className={classes["product-details__name"]}>{name}</h1>
-          <Rating productRating={rating} />
+          <Rating productRating={rating ?? 0} />
           <div className={classes["product-details__info"]}>
             <strong>{productAmountWithCurrency}</strong>
             <strong>
@@ -56,9 +69,13 @@ export default function Product({ product }: ProductBySlugQuery) {
             })}
           </>
           <p className={classes["product-details__availability"]}>
-            {isAvailable ? "in stock" : "Unavailable"}
+            {isAvailable ? (
+              <span className={classes["product-available"]}>in stock</span>
+            ) : (
+              <span className={classes['product-unavailable']}>unavailable</span>
+            )}
           </p>
-          {productPrice?.amount ? <CartButton /> : ""}
+          {productPrice?.amount ? <Button onClick={onAddToCart}>Add to cart</Button> : ""}
         </div>
       </article>
     </section>
