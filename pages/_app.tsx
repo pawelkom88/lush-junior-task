b84445e69551d3b4735958afb5d20df710135187
pawelkom88e <pawel.komorkiewicz@yahoo.co.uk>
2023-03-05 22:49:40 +0000

    feat: Add CheckoutCreateMutation

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,20 +1,51 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import type { AppProps } from "next/app";
 import { ApolloProvider } from "@apollo/client";
 import { client } from "./client/client";
 import Navigation from "@components/navigation/Navigation";
 import Footer from "@components/footer/Footer";
 import FooterMobile from "@components/footer/FooterMobile";
+import { useLocalStorage } from "react-use";
+import { useCheckoutCreateMutation, useCheckoutFetchAddedProductsQuery } from "@generated/api";
 import "@fontsource/figtree";
 import "@styles/globals.scss";
 
-export default function App({ Component, pageProps }: AppProps) {
+function Root({ Component, pageProps }: AppProps) {
   const [productQuery, setProductQuery] = useState<string>("");
+  const [token, setToken] = useLocalStorage<string>("token");
+  const [checkoutCreate] = useCheckoutCreateMutation();
+
+  const { data } = useCheckoutFetchAddedProductsQuery({
+    variables: { checkoutToken: token },
+    skip: !token,
+  });
+
+  useEffect(() => {
+    async function doCheckout() {
+      const { data } = await checkoutCreate();
+      const token: string = data?.checkoutCreate?.checkout?.token;
+
+      setToken(token);
+    }
+
+    doCheckout();
+  }, [checkoutCreate, setToken]);
 
   return (
+    <>
+      <Navigation
+        onProductQuery={setProductQuery}
+        checkoutProductsTotal={data?.checkout?.lines?.length ?? 0}
+      />
+      <Component {...pageProps} keyword={productQuery} token={token} />;
+    </>
+  );
+}
+
+export default function App(props: AppProps) {
+  return (
     <ApolloProvider client={client}>
-      <Navigation onProductQuery={setProductQuery} />
-      <Component {...pageProps} keyword={productQuery} />
+      <Root {...props} />
       <Footer />
       <FooterMobile />
     </ApolloProvider>
