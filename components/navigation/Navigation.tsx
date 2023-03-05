7f7a8e7ed0887f8154d0cadda072a791f780765e
pawelkom88e <pawel.komorkiewicz@yahoo.co.uk>
2023-03-05 22:49:09 +0000

    feat: Add data atribute to display number of product in cart

--- components/navigation/Navigation.tsx
+++ components/navigation/Navigation.tsx
@@ -1,18 +1,19 @@
 import { useState } from "react";
 import SearchBar from "@components/search-bar/SearchBar";
 import ShoppingBagIcon from "@root/public/icons/ShoppingBagIcon";
-
-import classes from "./Navigation.module.scss";
 import SrButton from "@components/sr-button/SrButton";
 import SearchIcon from "@components/search-icon/SearchIcon";
 import NavLeft from "./nav-left/NavLeft";
 import NavBottom from "./nav-bottom/NavBottom";
 import NavCenter from "./nav-center/NavCenter";
+import classes from "./Navigation.module.scss";
 
 export default function Navigation({
-  onProductQuery: onSetKeyword,
+  onProductQuery,
+  checkoutProductsTotal,
 }: {
   onProductQuery: (val: string) => void;
+  checkoutProductsTotal: number;
 }) {
   const [showSearchField, setShowSearchField] = useState<boolean>(false);
 
@@ -27,12 +28,13 @@ export default function Navigation({
         <div className={classes["navigation-right"]}>
           <SearchBar
             onShowSearchField={() => setShowSearchField(false)}
-            onSetKeyword={onSetKeyword}
+            onSetKeyword={onProductQuery}
             isDisplayed={showSearchField}
           />
           <SearchIcon onShowSearchField={() => setShowSearchField(true)} />
           <SrButton text="Cart">
             <ShoppingBagIcon size={24} />
+            <span className={classes.bag} data-count={checkoutProductsTotal}></span>
           </SrButton>
         </div>
       </header>
